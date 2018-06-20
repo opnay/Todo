08e41879b310c87d52c0e56c01db7a802ba50355
Kim In Seop <opnayn@gmail.com>
2018-06-20 13:13:40 +0900

    MainActivity: Use CoordinatorLayout

--- app/src/main/java/com/opnay/todo/MainActivity.kt
+++ app/src/main/java/com/opnay/todo/MainActivity.kt
@@ -6,6 +6,7 @@ import android.content.Intent
 import android.content.SharedPreferences
 import android.content.res.Configuration
 import android.os.Bundle
+import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity
 import android.text.Editable
 import android.text.TextWatcher
@@ -20,6 +21,7 @@ import com.opnay.todo.adapter.TodoAdapter
 import com.opnay.todo.data.TodoData
 import com.opnay.todo.preference.TodoPreference
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.content_main.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -37,6 +39,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setSupportActionBar(toolbar)
 
         main_add.setOnClickListener {
             showNewAdd(true)
