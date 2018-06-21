404b25b580e5d9cfb780cd733556bdafc6431cfa
Kim In Seop <opnayn@gmail.com>
2018-06-21 18:45:57 +0900

    Cleanup: Optimize import and remove unused code

--- app/src/main/java/com/opnay/todo/MainActivity.kt
+++ app/src/main/java/com/opnay/todo/MainActivity.kt
@@ -2,16 +2,12 @@ package com.opnay.todo
 
 import android.app.Activity
 import android.content.Context
-import android.content.Intent
 import android.content.SharedPreferences
 import android.content.res.Configuration
 import android.os.Bundle
-import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity
 import android.text.Editable
 import android.text.TextWatcher
-import android.util.TypedValue
-import android.view.KeyEvent
 import android.view.View
 import android.view.inputmethod.InputMethodManager
 import android.widget.Toast
