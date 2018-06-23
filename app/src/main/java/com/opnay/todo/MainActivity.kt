850164be78a236b26d09b3902214f74aef927cf4
Kim In Seop <opnayn@gmail.com>
2018-06-24 02:34:20 +0900

    MainActivity: Use ViewHolder and Rename id - Same reason with commit(18989a8)

--- app/src/main/java/com/opnay/todo/MainActivity.kt
+++ app/src/main/java/com/opnay/todo/MainActivity.kt
@@ -3,17 +3,22 @@ package com.opnay.todo
 import android.content.Context
 import android.content.res.Configuration
 import android.os.Bundle
+import android.support.constraint.ConstraintLayout
 import android.support.v7.app.AppCompatActivity
 import android.text.Editable
 import android.text.TextWatcher
 import android.view.View
 import android.view.inputmethod.InputMethodManager
+import android.widget.ListView
+import android.widget.TextView
 import android.widget.Toast
+import at.markushi.ui.CircleButton
 import com.opnay.todo.adapter.TodoAdapter
 import com.opnay.todo.data.TodoData
 import com.opnay.todo.preference.TodoPreference
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.content_main.*
+import ru.dimorinny.floatingtextbutton.FloatingTextButton
 
 class MainActivity : AppCompatActivity() {
     private var addNew: Boolean = false
@@ -24,41 +29,48 @@ class MainActivity : AppCompatActivity() {
         getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
     }
 
+    // View Holder
+    private val lstTodo: ListView by lazy { main_list as ListView }
+    private val fabAdd: FloatingTextButton by lazy { fab_btn_add as FloatingTextButton }
+    private val layNew: ConstraintLayout by lazy { new_item as ConstraintLayout }
+    private val btnNew: CircleButton by lazy { new_btn as CircleButton }
+    private val tvNew: TextView by lazy { new_title as TextView }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
 
-        main_add.setOnClickListener {
+        fabAdd.setOnClickListener {
             showNewAdd(true)
-            new_title.requestFocus()
+            tvNew.requestFocus()
             imm.toggleSoftInput(InputMethodManager.HIDE_IMPLICIT_ONLY, 1)
         }
 
-        new_btn.isEnabled = false
-        new_btn.setOnClickListener {
+        btnNew.isEnabled = false
+        btnNew.setOnClickListener {
             TodoData().apply {
-                title = new_title.text.toString()
+                title = tvNew.text.toString()
             }.run {
                 TodoPreference.prefData.add(this)
                 adapter.notifyDataSetChanged()
             }
-            main_list.smoothScrollByOffset(main_list.bottom)
+            lstTodo.smoothScrollByOffset(lstTodo.bottom)
 
-            new_title.text.clear()
+            tvNew.text = ""
             showNewAdd(false)
             imm.toggleSoftInput(InputMethodManager.HIDE_IMPLICIT_ONLY, 0)
         }
-        new_title.addTextChangedListener(object : TextWatcher {
+        tvNew.addTextChangedListener(object : TextWatcher {
             override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
             override fun afterTextChanged(s: Editable?) {}
             override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
-                new_btn.isEnabled = new_title.text.isNotEmpty()
+                btnNew.isEnabled = tvNew.text.isNotEmpty()
             }
         })
 
         TodoPreference.loadPref(this)
-        main_list.adapter = adapter
+        lstTodo.adapter = adapter
     }
 
     override fun onConfigurationChanged(newConfig: Configuration?) {
@@ -91,11 +103,11 @@ class MainActivity : AppCompatActivity() {
     private fun showNewAdd(en: Boolean) {
         addNew = en
         if (en) {
-            new_item.visibility = View.VISIBLE
-            main_add.visibility = View.GONE
+            layNew.visibility = View.VISIBLE
+            fabAdd.visibility = View.GONE
         } else {
-            new_item.visibility = View.GONE
-            main_add.visibility = View.VISIBLE
+            layNew.visibility = View.GONE
+            fabAdd.visibility = View.VISIBLE
         }
     }
 
