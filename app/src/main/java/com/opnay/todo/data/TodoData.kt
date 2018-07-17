84663bedee1a0c5770fb54962653568df7bd8274
Kim In Seop <opnayn@gmail.com>
2018-07-18 00:26:25 +0900

    TodoData: Add Category

--- app/src/main/java/com/opnay/todo/data/TodoData.kt
+++ app/src/main/java/com/opnay/todo/data/TodoData.kt
@@ -1,6 +1,8 @@
 package com.opnay.todo.data
 
-class TodoData(var title: String = "", var desc: String = "") {
+class TodoData(var title: String = "") {
+    var category: String = ""
+    var desc: String = ""
     var check: Boolean = false
 
     fun toggle(t: Boolean = !check) {
