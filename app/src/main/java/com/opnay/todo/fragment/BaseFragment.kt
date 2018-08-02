f1be2a5703d210163958a0039b9311ed2ef5c16f
Kim In Seop <opnayn@gmail.com>
2018-08-02 23:29:49 +0900

    BaseFragment: Add updateData inheritable function.

--- app/src/main/java/com/opnay/todo/fragment/BaseFragment.kt
+++ app/src/main/java/com/opnay/todo/fragment/BaseFragment.kt
@@ -8,4 +8,9 @@ open class BaseFragment: Fragment() {
      * @return  true    It's work. Don't super.onBackPressed on activity.
      */
     open fun onBackPressed(): Boolean { return false }
+
+    /*
+     * update Fragment's Data
+     */
+    open fun updateData() {}
 }
