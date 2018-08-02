a360be9cea9220f340705af0414b0ec06308caf1
Kim In Seop <opnayn@gmail.com>
2018-08-02 23:32:59 +0900

    MainActivity: Change type of fragment value to BaseFragment

--- app/src/main/java/com/opnay/todo/activity/MainActivity.kt
+++ app/src/main/java/com/opnay/todo/activity/MainActivity.kt
@@ -4,13 +4,14 @@ import android.os.Bundle
 import android.support.v7.app.ActionBar
 import android.support.v7.app.AppCompatActivity
 import com.opnay.todo.R
+import com.opnay.todo.fragment.BaseFragment
 import com.opnay.todo.fragment.ItemFragment
 import com.opnay.todo.preference.TodoPreference
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     // View Holder
-    private val fragment: ItemFragment = ItemFragment()
+    private val fragment: BaseFragment = ItemFragment()
     private val actionBar: ActionBar by lazy { supportActionBar!! }
 
     var title: String
