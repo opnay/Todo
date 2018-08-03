10bef69c6a65324e6000d64d164ef9162d05ff93
Kim In Seop <opnayn@gmail.com>
2018-08-03 23:49:23 +0900

    MainActivity: Change Default Fragment to CategoryFragment

--- app/src/main/java/com/opnay/todo/activity/MainActivity.kt
+++ app/src/main/java/com/opnay/todo/activity/MainActivity.kt
@@ -5,13 +5,15 @@ import android.support.v7.app.ActionBar
 import android.support.v7.app.AppCompatActivity
 import com.opnay.todo.R
 import com.opnay.todo.fragment.BaseFragment
+import com.opnay.todo.fragment.CategoryFragment
 import com.opnay.todo.fragment.ItemFragment
 import com.opnay.todo.preference.TodoPreference
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     // View Holder
-    private val fragment: BaseFragment = ItemFragment()
+//    private val fragment: BaseFragment = ItemFragment()
+    private val fragment: BaseFragment = CategoryFragment()
     private val actionBar: ActionBar by lazy { supportActionBar!! }
 
     var title: String
