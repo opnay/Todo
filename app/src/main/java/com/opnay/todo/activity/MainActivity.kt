c4078384bf58c1e6281f2a3e92fa9a3d95eac24a
Kim In Seop <opnayn@gmail.com>
2018-08-06 22:13:30 +0900

    MainActivity: background and theme move to MainActivity.kt

--- app/src/main/java/com/opnay/todo/activity/MainActivity.kt
+++ app/src/main/java/com/opnay/todo/activity/MainActivity.kt
@@ -1,7 +1,11 @@
 package com.opnay.todo.activity
 
+import android.graphics.Color
 import android.os.Bundle
+import android.support.v4.content.ContextCompat
 import android.support.v7.app.ActionBar
+import android.support.v7.widget.Toolbar
+import android.widget.LinearLayout
 import com.opnay.todo.R
 import com.opnay.todo.fragment.CategoryFragment
 import com.opnay.todo.preference.TodoPreference
@@ -10,6 +14,8 @@ import kotlinx.android.synthetic.main.activity_with_fragment.*
 class MainActivity : BaseActivity() {
 
     private val actionBar: ActionBar by lazy { supportActionBar!! }
+    private val rootView: LinearLayout by lazy { rootLayout }
+    private val toolBar: Toolbar by lazy { toolbar }
 
     var title: String
         get() = actionBar.title as String
@@ -20,7 +26,14 @@ class MainActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_with_fragment)
-        setSupportActionBar(toolbar)
+        setSupportActionBar(toolBar)
+
+        // Set Background
+        rootView.setBackgroundResource(R.drawable.main_background)
+        toolBar.apply {
+            setBackgroundColor(ContextCompat.getColor(this@MainActivity, android.R.color.transparent))
+            setTitleTextColor(Color.WHITE)
+        }
 
         fragment = CategoryFragment()
 
