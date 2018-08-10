ee30d817bc2653692d604de25680a56ba9d82cba
Kim In Seop <opnayn@gmail.com>
2018-08-11 02:09:49 +0900

    TodoData: Implement Parcelable

--- app/src/main/java/com/opnay/todo/data/TodoData.kt
+++ app/src/main/java/com/opnay/todo/data/TodoData.kt
@@ -1,11 +1,33 @@
 package com.opnay.todo.data
 
-class TodoData(var title: String = "") {
+import android.os.Parcel
+import android.os.Parcelable
+
+class TodoData(var title: String = "") : Parcelable {
     var category: String = ""
     var desc: String = ""
     var check: Boolean = false
 
     fun toggle(t: Boolean = !check) {
         check = t
+    }
+
+    constructor(parcel: Parcel) : this(parcel.readString()) {
+        category = parcel.readString()
+        desc = parcel.readString()
+        check = parcel.readByte() != 0.toByte()
+    }
+
+    override fun writeToParcel(parcel: Parcel, flags: Int) {
+        parcel.writeString(title)
+        parcel.writeString(category)
+        parcel.writeString(desc)
+        parcel.writeByte(if (check) 1 else 0)
+    }
+
+    override fun describeContents(): Int = 0
+    companion object CREATOR : Parcelable.Creator<TodoData> {
+        override fun createFromParcel(parcel: Parcel): TodoData = TodoData(parcel)
+        override fun newArray(size: Int): Array<TodoData?> = arrayOfNulls(size)
     }
 }
