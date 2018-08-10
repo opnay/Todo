4aeaaec4e32f64db20b1923ed9ba2a7342e5194c
Kim In Seop <opnayn@gmail.com>
2018-08-10 21:15:25 +0900

    Util: startActivity can get Parcelable

--- app/src/main/java/com/opnay/todo/Util.kt
+++ app/src/main/java/com/opnay/todo/Util.kt
@@ -2,14 +2,21 @@ package com.opnay.todo
 
 import android.content.Context
 import android.content.Intent
+import android.os.Parcel
+import android.os.Parcelable
 import java.io.Serializable
 
 class Util {
     companion object {
         fun startActivity(ctx: Context, cls: Class<*>) { startActivity(ctx, cls, null) }
-        fun startActivity(context: Context, cls: Class<*>, extra: HashMap<String, Serializable>?) {
+        fun startActivity(context: Context, cls: Class<*>, extra: HashMap<String, *>?) {
             Intent(context, cls).run {
-                extra?.forEach { k, d -> this.putExtra(k, d) }
+                extra?.forEach { k, d ->
+                    if (d is Parcelable)
+                        this.putExtra(k, d)
+                    else
+                        this.putExtra(k, d as Serializable)
+                }
                 context.startActivity(this)
             }
         }
