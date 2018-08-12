a31c8ba1be8918491c0989a5d20c0dfbd2e1b45a
Kim In Seop <opnayn@gmail.com>
2018-08-13 01:58:53 +0900

    Remove TodoPreference

--- app/src/main/java/com/opnay/todo/activity/SplashActivity.kt
+++ app/src/main/java/com/opnay/todo/activity/SplashActivity.kt
@@ -4,7 +4,6 @@ import android.app.Activity
 import android.os.Bundle
 import com.opnay.todo.R
 import com.opnay.todo.Util
-import com.opnay.todo.preference.TodoPreference
 import java.util.*
 import kotlin.concurrent.schedule
 
@@ -13,8 +12,6 @@ class SplashActivity : Activity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
-
-        TodoPreference.loadPref(this)
 
         Timer().schedule(2000) {
             Util.startActivity(this@SplashActivity, MainActivity::class.java)
