b7495031cbe6698bd211b1fa95a1fdeaefe429b7
Kim In Seop <opnayn@gmail.com>
2018-08-12 15:05:20 +0900

    sqlite: Add ItemDatabaseHelper. - use Context.db

--- app/src/main/java/com/opnay/todo/sqlite/ItemDatabaseHelper.kt
+++ app/src/main/java/com/opnay/todo/sqlite/ItemDatabaseHelper.kt
@@ -2,13 +2,15 @@ package com.opnay.todo.sqlite
 
 import android.content.Context
 import android.database.sqlite.SQLiteDatabase
+import com.opnay.todo.data.TodoData
+import com.opnay.todo.toBoolean
 import org.jetbrains.anko.db.*
 
 val Context.db: ItemDatabaseHelper
     get() = ItemDatabaseHelper.getInstance(applicationContext)
 
 class ItemDatabaseHelper(ctx: Context):
-        ManagedSQLiteOpenHelper(ctx, "ItemDatabase", null, 1) {
+        ManagedSQLiteOpenHelper(ctx, "ItemDB", null, 1) {
 
     companion object {
         private var instance: ItemDatabaseHelper? = null
@@ -17,24 +19,38 @@ class ItemDatabaseHelper(ctx: Context):
         fun getInstance(ctx: Context): ItemDatabaseHelper =
                 instance ?: ItemDatabaseHelper(ctx).also { instance = it }
 
-        const val TABLE = "ITEM"
+        const val TABLE_CAT = "CATEGORY"
+        const val TABLE_ITEM = "ITEM"
+
         const val ATTR_ID = "ID"
         const val ATTR_TITLE = "TITLE"
         const val ATTR_CATEGORY = "CATEGORY"
         const val ATTR_DESC = "DESC"
-        const val ATTR_CHECK = "CHECK"
+        const val ATTR_COMPLETE = "COMPLETE"
+
+        val TodoParser: MapRowParser<TodoData> = object: MapRowParser<TodoData> {
+            override fun parseRow(columns: Map<String, Any?>): TodoData =
+                    TodoData((columns.getValue(ItemDatabaseHelper.ATTR_ID) as Long).toInt(),
+                        columns.getValue(ItemDatabaseHelper.ATTR_TITLE) as String,
+                        columns.getValue(ItemDatabaseHelper.ATTR_CATEGORY) as String,
+                        columns.getValue(ItemDatabaseHelper.ATTR_DESC) as String,
+                        (columns.getValue(ItemDatabaseHelper.ATTR_COMPLETE) as Long).toBoolean())
+        }
     }
 
     override fun onCreate(db: SQLiteDatabase) {
-        db.createTable(TABLE, true,
+        db.createTable(TABLE_ITEM, true,
                 ATTR_ID to INTEGER + PRIMARY_KEY + UNIQUE,
                 ATTR_TITLE to TEXT,
                 ATTR_CATEGORY to TEXT,
                 ATTR_DESC to TEXT,
-                ATTR_CHECK to INTEGER)
+                ATTR_COMPLETE to INTEGER)
+
+        db.createTable(TABLE_CAT, true,
+                ATTR_ID to INTEGER + PRIMARY_KEY + UNIQUE,
+                ATTR_TITLE to TEXT)
     }
 
     // Yet use
     override fun onUpgrade(db: SQLiteDatabase?, oldVer: Int, newVer: Int) {}
-
 }
