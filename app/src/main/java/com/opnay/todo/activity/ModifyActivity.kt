00a00356b68553178e046eaa31c54dcd4a134f4d
Kim In Seop <opnayn@gmail.com>
2018-08-14 03:52:54 +0900

    ModifyActivity: Replace spinner with toolbar

--- app/src/main/java/com/opnay/todo/activity/ModifyActivity.kt
+++ app/src/main/java/com/opnay/todo/activity/ModifyActivity.kt
@@ -1,9 +1,12 @@
 package com.opnay.todo.activity
 
+import android.graphics.Color
 import android.os.Bundle
 import android.support.design.widget.Snackbar
 import android.support.design.widget.TextInputEditText
+import android.support.v4.content.ContextCompat
 import android.support.v7.app.AppCompatActivity
+import android.support.v7.widget.Toolbar
 import android.widget.Button
 import android.widget.EditText
 import com.opnay.todo.R
@@ -15,16 +18,8 @@ import kotlinx.android.synthetic.main.activity_modify.*
 class ModifyActivity : AppCompatActivity() {
     private val data: TodoData by lazy { intent.getParcelableExtra(Util.KEY_ITEM) as TodoData }
 
-//    private val spinAdapter by lazy {
-//        ArrayAdapter<String>(this, R.layout.category_spinner_checked)
-//                .apply {
-//                    setDropDownViewResource(R.layout.category_spinner)
-//                    addAll(TodoPreference.catData)
-//                }
-//    }
-
     // View Holder
-//    private val spinCategory: Spinner by lazy { spin_category as Spinner }
+    private val toolBar: Toolbar by lazy { toolbar }
     private val tvTitle: EditText by lazy { edit_title }
     private val tvDesc: TextInputEditText by lazy { edit_desc.editText as TextInputEditText }
     private val btnOK: Button by lazy { btn_ok }
@@ -33,6 +28,19 @@ class ModifyActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_modify)
+        setSupportActionBar(toolBar)
+
+        supportActionBar!!.apply {
+            // Enable back button
+            setDisplayHomeAsUpEnabled(true)
+            setDisplayShowHomeEnabled(true)
+            title = this@ModifyActivity.db.category.find { it.id == data.category }!!.title
+        }
+
+        toolBar.apply {
+            setBackgroundColor(ContextCompat.getColor(this@ModifyActivity, android.R.color.transparent))
+            setTitleTextColor(Color.WHITE)
+        }
 
         btnOK.setOnClickListener {
             if (tvTitle.text.isEmpty()) {
@@ -45,7 +53,6 @@ class ModifyActivity : AppCompatActivity() {
                 data.apply {
                     title = tvTitle.text.toString()
                     desc = tvDesc.text.toString()
-//            category = spinCategory.selectedItem.toString()
                 }.update(this)
 
                 finish()
@@ -57,16 +64,13 @@ class ModifyActivity : AppCompatActivity() {
             finish()
         }
 
-//        spinCategory.adapter = spinAdapter
-
         // Load Data
         data.run {
             tvTitle.setText(title)
             tvDesc.setText(desc)
-//            spinAdapter.getPosition(category).let {
-//                spinCategory.setSelection(maxOf(it, 0))
-//            }
         }
     }
 
+    // Toolbar Back Button
+    override fun onSupportNavigateUp(): Boolean = true.also { onBackPressed() }
 }
