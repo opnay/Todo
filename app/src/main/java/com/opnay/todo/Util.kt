a1c1651a22fcb2bfa30db3fa4b58c8d6697a2562
Kim In Seop <opnayn@gmail.com>
2018-08-15 23:16:17 +0900

    Util: Add Context.getTintedDrawable

--- app/src/main/java/com/opnay/todo/Util.kt
+++ app/src/main/java/com/opnay/todo/Util.kt
@@ -1,5 +1,11 @@
 package com.opnay.todo
 
+import android.content.Context
+import android.graphics.Color
+import android.graphics.drawable.Drawable
+import android.support.v4.content.ContextCompat
+import android.support.v4.graphics.drawable.DrawableCompat
+
 class Util {
     companion object {
         const val KEY_CATEGORY = "CATEGORY"
@@ -15,3 +21,8 @@ fun Int.toBoolean(): Boolean = (this != 0)
 
 fun Boolean.toByte(): Byte = if (this) 1 else 0
 fun Byte.toBoolean(): Boolean = (this != 0.toByte())
+
+fun Context.getTintedDrawable(id: Int, color: Int = Color.BLACK) =
+    ContextCompat.getDrawable(this, R.drawable.ic_keyboard_arrow_left_black_24dp)!!.apply {
+        DrawableCompat.setTint(this, color)
+    }
--- app/src/main/java/com/opnay/todo/view/ActionBar.kt
+++ app/src/main/java/com/opnay/todo/view/ActionBar.kt
@@ -1,11 +1,10 @@
 package com.opnay.todo.view
 
 import android.graphics.Color
-import android.support.v4.content.ContextCompat
-import android.support.v4.graphics.drawable.DrawableCompat
 import android.support.v7.app.ActionBar
 import com.opnay.todo.R
 import com.opnay.todo.data.Category
+import com.opnay.todo.getTintedDrawable
 
 fun ActionBar.setTitle(category: Category) {
     this.title = category.title.toUpperCase()
@@ -15,9 +14,5 @@ fun ActionBar.enableBack(en: Boolean, color: Int = Color.BLACK) {
     setDisplayHomeAsUpEnabled(en)
     setDisplayShowHomeEnabled(en)
 
-    val drawable =
-            ContextCompat.getDrawable(themedContext, R.drawable.ic_keyboard_arrow_left_black_24dp)!!
-
-    DrawableCompat.setTint(drawable, color)
-    setHomeAsUpIndicator(drawable)
+    setHomeAsUpIndicator(themedContext.getTintedDrawable(R.drawable.ic_keyboard_arrow_left_black_24dp, color))
 }
