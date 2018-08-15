3cbcc8084d8c9acef3147279a9020ae1cdf22019
Kim In Seop <opnayn@gmail.com>
2018-08-15 19:14:36 +0900

    ModifyActivity: Convert to BaseActivity and divide to BaseFragment.

--- app/src/main/java/com/opnay/todo/activity/ModifyActivity.kt
+++ app/src/main/java/com/opnay/todo/activity/ModifyActivity.kt
@@ -2,35 +2,19 @@ package com.opnay.todo.activity
 
 import android.graphics.Color
 import android.os.Bundle
-import android.support.design.widget.Snackbar
-import android.support.design.widget.TextInputEditText
 import android.support.v4.content.ContextCompat
-import android.support.v7.app.AppCompatActivity
-import android.support.v7.widget.Toolbar
-import android.widget.Button
-import android.widget.EditText
-import com.opnay.todo.R
 import com.opnay.todo.Util
 import com.opnay.todo.data.TodoData
+import com.opnay.todo.fragment.ModifyFragment
 import com.opnay.todo.sqlite.db
 import com.opnay.todo.view.enableBack
 import com.opnay.todo.view.setTitle
-import kotlinx.android.synthetic.main.activity_modify.*
 
-class ModifyActivity : AppCompatActivity() {
+class ModifyActivity : BaseActivity() {
     private val data: TodoData by lazy { intent.getParcelableExtra(Util.KEY_ITEM) as TodoData }
-
-    // View Holder
-    private val toolBar: Toolbar by lazy { toolbar }
-    private val tvTitle: EditText by lazy { edit_title }
-    private val tvDesc: TextInputEditText by lazy { edit_desc.editText as TextInputEditText }
-    private val btnOK: Button by lazy { btn_ok }
-    private val btnDel: Button by lazy { btn_del }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_modify)
-        setSupportActionBar(toolBar)
 
         supportActionBar!!.apply {
             // Enable back button
@@ -43,33 +27,9 @@ class ModifyActivity : AppCompatActivity() {
             setTitleTextColor(Color.WHITE)
         }
 
-        btnOK.setOnClickListener {
-            if (tvTitle.text.isEmpty()) {
-                Snackbar.make(layout_todo, "제목을 다시 입력해주세요.", Snackbar.LENGTH_LONG)
-                        .setAction("OK") { tvTitle.requestFocus() }
-                        .show()
-                return@setOnClickListener
-            } else {
-                // Save to db
-                data.apply {
-                    title = tvTitle.text.toString()
-                    desc = tvDesc.text.toString()
-                }.update(this)
-
-                finish()
-            }
-        }
-
-        btnDel.setOnClickListener {
-            db.deleteItem(data.id)
-            finish()
-        }
-
-        // Load Data
-        data.run {
-            tvTitle.setText(title)
-            tvDesc.setText(desc)
-        }
+        supportFragmentManager.replace(ModifyFragment().also {
+            it.arguments = Bundle().apply { putParcelable(Util.KEY_ITEM, data) }
+        })
     }
 
     // Toolbar Back Button
---
+++ app/src/main/java/com/opnay/todo/fragment/ModifyFragment.kt
@@ -0,0 +1,63 @@
+package com.opnay.todo.fragment
+
+import android.os.Bundle
+import android.support.design.widget.Snackbar
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
+import com.opnay.todo.R
+import com.opnay.todo.Util
+import com.opnay.todo.activity.BaseActivity
+import com.opnay.todo.data.TodoData
+import kotlinx.android.synthetic.main.fragment_modify.view.*
+
+class ModifyFragment: BaseFragment() {
+    private val parent: BaseActivity by lazy { activity!! as BaseActivity }
+
+    // Data
+    private val data: TodoData by lazy { arguments!!.getParcelable<TodoData>(Util.KEY_ITEM) }
+
+    // View Holder
+    private var rootView: View? = null
+    private val tvTitle: EditText by lazy { rootView!!.edit_title }
+    private val tvDesc: EditText by lazy { rootView!!.edit_desc }
+    private val btnOK: Button by lazy { rootView!!.btn_ok }
+    private val btnDel: Button by lazy { rootView!!.btn_del }
+
+    override fun onCreateView(inflater: LayoutInflater,
+                              container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        rootView = inflater.inflate(R.layout.fragment_modify, container, false)
+
+        btnOK.setOnClickListener {
+            if (tvTitle.text.isEmpty()) {
+                Snackbar.make(rootView!!, "제목을 다시 입력해주세요.", Snackbar.LENGTH_LONG)
+                        .setAction("OK") { tvTitle.requestFocus() }
+                        .show()
+                return@setOnClickListener
+            } else {
+                // Save to db
+                data.apply {
+                    title = tvTitle.text.toString()
+                    desc = tvDesc.text.toString()
+                }.update(parent)
+
+                parent.finish()
+            }
+        }
+
+        btnDel.setOnClickListener {
+            data.delete(parent)
+            parent.finish()
+        }
+
+        // Load Data
+        data.run {
+            tvTitle.setText(title)
+            tvDesc.setText(desc)
+        }
+        return rootView
+    }
+}
