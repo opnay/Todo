e14f3efebb1d78073061c6f084a9fabd4974890a
Kim In Seop <opnayn@gmail.com>
2018-08-15 18:00:49 +0900

    view/ActionBar: Set color for back button

--- app/src/main/java/com/opnay/todo/view/ActionBar.kt
+++ app/src/main/java/com/opnay/todo/view/ActionBar.kt
@@ -1,5 +1,8 @@
 package com.opnay.todo.view
 
+import android.graphics.Color
+import android.support.v4.content.ContextCompat
+import android.support.v4.graphics.drawable.DrawableCompat
 import android.support.v7.app.ActionBar
 import com.opnay.todo.R
 import com.opnay.todo.data.Category
@@ -8,8 +11,13 @@ fun ActionBar.setTitle(category: Category) {
     this.title = category.title.toUpperCase()
 }
 
-fun ActionBar.enableBack(en: Boolean) {
+fun ActionBar.enableBack(en: Boolean, color: Int = Color.BLACK) {
     setDisplayHomeAsUpEnabled(en)
     setDisplayShowHomeEnabled(en)
-    setHomeAsUpIndicator(R.drawable.ic_keyboard_arrow_left_black_24dp)
+
+    val drawable =
+            ContextCompat.getDrawable(themedContext, R.drawable.ic_keyboard_arrow_left_black_24dp)!!
+
+    DrawableCompat.setTint(drawable, color)
+    setHomeAsUpIndicator(drawable)
 }
